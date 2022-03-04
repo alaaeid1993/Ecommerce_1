38f52f783228cd5475572bbc933544a9237f2865
Alaa Eid <70823641+alaaeid1993@users.noreply.github.com>
2022-03-04 22:12:19 +0200

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MainComponent } from './Components/main/main.component';
+import { ProductComponent } from './Components/product/product.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { ErrorComponent } from './Components/error/error.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { SlideShowComponent } from './Components/slide-show/slide-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    ProductComponent,
+    FooterComponent,
+    RegistrationComponent,
+    LogInComponent,
+    ErrorComponent,
+    ContactComponent,
+    SlideShowComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
